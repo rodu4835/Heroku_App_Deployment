8b2584dc2609cfab7fdda2472c7d2289bacbb54b
JSimsProgrammer <67485527+JSimsProgrammer@users.noreply.github.com>
2022-02-17 15:30:45 -0600

    Add Deck
    Added the decks to the Console file

---
+++ ConsoleWar.py
@@ -0,0 +1,21 @@
+#This is the protoype of the war game to run on console. - JS
+
+# Below, I started the deck. I figured the best way to program it would be a list of tuples, with the key being the card designation
+# and the value being the value of the card (1-13 per suit) - JS
+
+officialDeck = [
+    # Hearts
+    ('A-H', 1), ('2-H', 2), ('3-H', 3), ('4-H', 4), ('5-H', 5), ('6-H', 6), ('7-H', 7), ('8-H', 8), ('9-H', 9), ('10-H', 10), ('J-H', 11), 
+    ('Q-H', 12), ('K-H', 13),
+    # Spades
+    ('A-S', 1), ('2-S', 2), ('3-S', 3), ('4-S', 4), ('5-S', 5), ('6-S', 6), ('7-S', 7), ('8-S', 8), ('9-S', 9), ('10-S', 10), ('J-S', 11), 
+    ('Q-S', 12), ('K-S', 13),
+    # Diamonds
+    ('A-D', 1), ('2-D', 2), ('3-D', 3), ('4-D', 4), ('5-D', 5), ('6-D', 6), ('7-D', 7), ('8-D', 8), ('9-D', 9), ('10-D', 10), ('J-D', 11), 
+    ('Q-D', 12), ('K-D', 13),
+    # Clubs
+    ('A-C', 1), ('2-C', 2), ('3-C', 3), ('4-C', 4), ('5-C', 5), ('6-C', 6), ('7-C', 7), ('8-C', 8), ('9-C', 9), ('10-C', 10), ('J-C', 11), 
+    ('Q-C', 12), ('K-C', 13)
+]
+
+print(len(officialDeck))
